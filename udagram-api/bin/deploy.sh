eaa9693cb00459cfada4d133d815bf9958026f7d
7osCraft <6911267+7osCraft@users.noreply.github.com>
2022-05-06 21:44:58 +0200

    Backend deploy

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,1 +1,1 @@
-echo "Hello"
+eb deploy
--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "http://udacitydeployment-env.eba-rsnnmdsa.eu-west-3.elasticbeanstalk.com/api/v0",
 };
 
 /*
